01f97702e30cf8b2ad6374113c59627dbfc0dd0a
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-02 15:21:18 +0545

    Removed excessive connection check.

--- Database.php
+++ Database.php
@@ -12,7 +12,6 @@ use PDOException;
 class Database
 {
     public ?PDO $connection = null;
-    private ?string $query = null;
     private null|bool|PDOStatement $queryStatement = null;
     private bool $queryExecutionStatus = false;
 
@@ -45,9 +44,10 @@ class Database
             // Define the DB_DSN, DB_USER, and DB_PASS in the /config/database.php file of the project/root directory.
             $this->connection = new PDO(DB_DSN, DB_USER, DB_PASS, $pdoAttributes);
         } catch (Throwable $th) {
-            $this->throwError("NGFramerPHPDbService/Database.php/connect() :: Connection to the database was not established. Error Message: " . $th->getMessage());
+            $this->throwError("Connection to the database was not established. Error Message: " . $th->getMessage());
         }
     }
+
 
     /**
      * @throws Exception
@@ -56,9 +56,10 @@ class Database
     {
         $configFile = ROOT . '/config/database.php';
         if (!file_exists($configFile)) {
-            $this->throwError("NGFramerPHPDbService/Database.php/validateConfigFile() :: Missing Connection Configuration File. Please check /config/database.php.");
+            $this->throwError("Missing Connection Configuration File. Please check /config/database.php.");
         } else require_once $configFile;
     }
+
 
     private function throwError(string $message): void
     {
@@ -66,29 +67,31 @@ class Database
         throw new PDOException($message);
     }
 
+
     private function logError(string $message): void
     {
         $logFile = ROOT . '/logs/database_errors.log';
         error_log(date("[Y-m-d H:i:s]") . " " . $message . PHP_EOL, 3, $logFile);
     }
 
+
     public function prepare(string $queryStatement = null, $options = []): ?static
     {
-        if (!$this->connection) $this->throwError("ngframerphp/core/Database.php/prepare() :: No database connection to prepare the statement.");
-        elseif (empty($queryStatement)) $this->throwError("ngframerphp/core/Database.php/prepare() :: No query to prepare.");
+        if (empty($queryStatement)) $this->throwError("No / empty query to prepare.");
         else $this->queryStatement = $this->connection->prepare($queryStatement, $options);
         return $this;
     }
+
 
     public function bindParams(array &$args): ?static
     {
         // If queryStatement is false, then throw an exception.
         if (!$this->queryStatement) {
-            $this->throwError("ngframerphp/core/Database.php/bindParams() :: No queryStatement to bind parameters.");
+            $this->throwError("No queryStatement to bind parameters to.");
         }
         // If there are no parameters to bind to the statement.
         if (count($args) === 0) {
-            $this->throwError("ngframerphp/core/Database.php/bindParams() :: No parameters to bind to queryStatement.");
+            $this->throwError("No parameters passed to bind to queryStatement.");
         }
         // If all elements of the array are arrays, then run bindParam function with arg.
         if ($this->areAllElementsArray($args)) {
@@ -98,8 +101,7 @@ class Database
                 $type = $args['type'] ?? $args[2] ?? PDO::PARAM_STR;
                 $this->bindParam($name, $value, $type);
             }
-        }
-        // If all the elements of thr array ($args) are not array/s.
+        } // If all the elements of thr array ($args) are not array/s.
         else {
             $name = $args['name'] ?? $args[0];
             $value = $args['value'] ?? $args[1];
@@ -110,6 +112,7 @@ class Database
         return $this;
     }
 
+
     public function bindParam($name, &$value, $type = PDO::PARAM_STR): static
     {
         if (!str_starts_with($name, ":")) $name = ":" . $name;
@@ -117,15 +120,16 @@ class Database
         return $this;
     }
 
+
     public function bindValues(array &$args): ?static
     {
         // If queryStatement is false, then throw an exception.
         if (!$this->queryStatement) {
-            $this->throwError("ngframerphp/core/Database.php/bindParams() :: No queryStatement to bind parameters.");
+            $this->throwError("No queryStatement to bind parameters to.");
         }
         // If there are no parameters to bind to the statement.
         if (count($args) === 0) {
-            $this->throwError("ngframerphp/core/Database.php/bindParams() :: No parameters to bind to queryStatement.");
+            $this->throwError("No parameters passed to bind to queryStatement.");
         }
         // If all elements of the array are arrays, then run bindParam function with arg.
         if ($this->areAllElementsArray($args)) {
@@ -135,8 +139,7 @@ class Database
                 $type = $args['type'] ?? $args[2] ?? PDO::PARAM_STR;
                 $this->bindValue($name, $value, $type);
             }
-        }
-        // If all the elements of thr array ($args) are not array/s.
+        } // If all the elements of thr array ($args) are not array/s.
         else {
             $name = $args['name'] ?? $args[0];
             $value = $args['value'] ?? $args[1];
@@ -147,12 +150,14 @@ class Database
         return $this;
     }
 
+
     public function bindValue($name, $value, $type = PDO::PARAM_STR): static
     {
         if (!str_starts_with($name, ":")) $name = ":" . $name;
         $this->queryStatement->bindParam($name, $value, $type);
         return $this;
     }
+
 
     private function areAllElementsArray(array $args): bool
     {
@@ -162,46 +167,45 @@ class Database
         return true;
     }
 
+
     public function execute(string $queryStatement = null): static
     {
         // If queryStatement is not null, then execute the queryStatement.
         if ($queryStatement !== null && $this->queryStatement === null) {
             // query() function prepares and executes the queryStatement, and returns pdoStatement|false.
+
             $this->queryStatement = $this->connection->query($queryStatement);
-            $this->queryExecutionStatus = ($this->queryStatement !== false) ? true : false;
+            $this->queryExecutionStatus = $this->queryStatement !== false;
         } // If queryStatement is true, then execute the queryStatement.
         else if ($queryStatement === null && $this->queryStatement !== null) {
             $this->queryExecutionStatus = $this->queryStatement->execute();
         } // If queryStatement is false, throw an error.
-        else $this->throwError("ngframerphp/core/Database.php/bindParam() :: Invalid or no queryStatement to execute.");
+        else $this->throwError("Invalid or no queryStatement to execute.");
+
         return $this;
     }
 
 
     public function beginTransaction(): bool
     {
-        if (!$this->connection) $this->throwError("ngframerphp/core/Database.php/beginTransaction() :: No database connection");
         return $this->connection->beginTransaction();
     }
 
 
     public function commit(): bool
     {
-        if (!$this->connection) $this->throwError("ngframerphp/core/Database.php/commitTransaction() :: No database connection");
         return $this->connection->commit();
     }
 
 
     public function rollback(): bool
     {
-        if (!$this->connection) $this->throwError("ngframerphp/core/Database.php/rollBackTransaction() :: No database connection");
         return $this->connection->rollBack();
     }
 
 
     public function lastInsertId(): string
     {
-        if (!$this->connection) $this->throwError("ngframerphp/core/Database.php/lastInsertId() :: No database connection");
         return $this->connection->lastInsertId();
     }
 
@@ -209,14 +213,14 @@ class Database
     public function fetch($fetchStyle = PDO::FETCH_ASSOC)
     {
         if ($this->queryExecutionStatus) return $this->queryStatement->fetch($fetchStyle);
-        else $this->throwError("ngframerphp/core/Database.php/fetch() :: No executed statement to fetch results");
+        else $this->throwError("No executed statement to fetch results");
     }
 
 
     public function fetchAll($fetchStyle = PDO::FETCH_ASSOC): bool|array
     {
         if ($this->queryExecutionStatus) return $this->queryStatement->fetchAll($fetchStyle);
-        else $this->throwError("ngframerphp/core/Database.php/fetchAll() :: No executed statement to fetch results");
+        else $this->throwError("No executed statement to fetch results");
     }
 
     public function close(): void

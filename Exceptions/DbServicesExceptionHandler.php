fb9d2d7889e90ca3d23651e7ffd5e2e5b1be5292
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-18 09:40:35 +0545

    Removal of error and exception handler package.

--- Exceptions/DbServicesExceptionHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace NGFramer\NGFramerPHPDbServices\Exceptions;
-
-use NGFramer\NGFramerPHPExceptions\handlers\ApiExceptionHandler;
-
-class DbServicesExceptionHandler extends ApiExceptionHandler
-{
-}
